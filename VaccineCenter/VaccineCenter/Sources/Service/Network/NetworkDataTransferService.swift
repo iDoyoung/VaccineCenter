54268f6bf37067fe13ec0546c0a6862d6b49f693
iDoyoung <ido.young@icloud.com>
2022-10-09 01:40:51 +0900

    Add Network Data Transfer Service

---
+++ VaccineCenter/VaccineCenter/Sources/Service/Network/NetworkDataTransferService.swift
@@ -0,0 +1,87 @@
+//
+//  NetworkDataTransferService.swift
+//  VaccineCenter
+//
+//  Created by Doyoung on 2022/10/08.
+//
+
+import Foundation
+import RxSwift
+
+enum DataTransferError: Error {
+    case noResponse
+    case parsing(Error)
+    case networkFailure(NetworkError)
+}
+
+protocol NetworkDataTransferServiceProtocol {
+    func reqeust<T: Decodable, E: ResponseRequestable>(with endpoint: E) -> Observable<T> where E.Response == T
+    func reqeust<E>(with endpoint: E) -> Observable<Data?> where E: Requestable
+}
+
+final class NetworkDataTransferService: NetworkDataTransferServiceProtocol {
+    
+    private let networkService: NetworkServiceProtocol
+    private var disposeBag = DisposeBag()
+    
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
+    }
+    
+    func reqeust<T: Decodable, E: ResponseRequestable>(with endpoint: E) -> Observable<T> where E.Response == T {
+        return Observable<Data?>.create { [weak self] observer in
+            if let self = self {
+                self.networkService.request(endpoint: endpoint) { result in
+                    switch result {
+                    case .success(let data):
+                        observer.onNext(data)
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                }
+            }
+            return Disposables.create()
+        }.map { data in
+            do {
+                let result: T = try self.decode(data: data)
+                return result
+            } catch let error {
+                throw error
+            }
+        }
+    }
+    
+    func reqeust<E>(with endpoint: E) -> Observable<Data?> where E: Requestable {
+        return Observable<Data?>.create { [weak self] observer in
+            self?.networkService.request(endpoint: endpoint) { result in
+                switch result {
+                case .success(let data):
+                    observer.onNext(data)
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    private func decode<T: Decodable>(data: Data?) throws -> T {
+        do {
+            guard let data = data else {
+                throw DataTransferError.noResponse
+            }
+            let result: T = try JSONResponseDecoder().decode(data)
+            return result
+        } catch {
+            throw DataTransferError.parsing(error)
+        }
+    }
+}
+
+final class JSONResponseDecoder {
+    private let jsonDecoder = JSONDecoder()
+    init() { }
+    func decode<T: Decodable>(_ data: Data) throws -> T {
+        return try jsonDecoder.decode(T.self, from: data)
+    }
+}
