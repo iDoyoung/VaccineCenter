fca7e8f724be57d24e6b265b5fcec7bb67bbd66c
iDoyoung <ido.young@icloud.com>
2022-10-09 09:42:32 +0900

    Add Model

---
+++ VaccineCenter/VaccineCenter/Sources/Model/VaccineCenterModel.swift
@@ -0,0 +1,23 @@
+//
+//  VaccineCenterModel.swift
+//  VaccineCenter
+//
+//  Created by Doyoung on 2022/10/09.
+//
+
+import Foundation
+
+enum VaccineCenterModel {
+    struct Response: Decodable {
+        let data: [Center]
+        
+        struct Center: Decodable {
+            let id: Int
+            let centerName: String
+            let facilityName: String
+            let address: String
+            let phoneNumber: String
+            let updatedAt: String
+        }
+    }
+}
