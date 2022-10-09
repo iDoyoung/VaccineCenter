a175bb2b2b40c1e85da71a3f8f8f639c6f9c1537
iDoyoung <ido.young@icloud.com>
2022-10-09 21:02:10 +0900

    Add Method to Bind View Model with table view

--- VaccineCenter/VaccineCenter/Sources/Scene/List/ListVaccineCenterViewController.swift
+++ VaccineCenter/VaccineCenter/Sources/Scene/List/ListVaccineCenterViewController.swift
@@ -11,12 +11,21 @@ import RxSwift
 import RxCocoa
 
 final class ListVaccineCenterViewController: UIViewController {
+    private var disposeBag = DisposeBag()
+    var viewModel: (ListVaccineCenterViewModelInput & ListVaccineCenterViewModelOutput)?
     //MARK: - UI Components
     let vaccineCenterTableView: UITableView = {
         let tableView = UITableView()
         return tableView
     }()
     //MARK: - Life Cycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.viewModel = ListVaccineCenterViewModel()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func loadView() {
         view = UIView()
         view.backgroundColor = .systemBackground
@@ -25,11 +34,20 @@ final class ListVaccineCenterViewController: UIViewController {
         super.viewDidLoad()
         setupNavigationBar()
         configureVaccineCenterTableView()
+        guard let viewModel = viewModel else { return }
+        viewModel.fetchFirstPage()
+        bind(to: viewModel)
     }
     //MARK: - Configure
     private func configureVaccineCenterTableView() {
         view.addSubview(vaccineCenterTableView)
         setupVaccineCenterTableViewLayoutConstraints()
+    }
+    private func bind(to viewModel: (ListVaccineCenterViewModelInput&ListVaccineCenterViewModelOutput)) {
+        viewModel.centers.bind(to: vaccineCenterTableView.rx.items(cellIdentifier: VaccineCenterCell.reuseIdenetifier, cellType: VaccineCenterCell.self)) { index, center, cell in
+            cell.vaccineCenter = center
+        }
+        .disposed(by: disposeBag)
     }
     //MARK: - Setup
     private func setupNavigationBar() {
