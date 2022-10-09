cfdb5380b2d96701ac52d0b0bac70e77b4c3d5d9
iDoyoung <ido.young@icloud.com>
2022-10-09 09:47:48 +0900

    Add API Endpoint

---
+++ VaccineCenter/VaccineCenter/Sources/Service/Network/APIEndpoint.swift
@@ -0,0 +1,14 @@
+//
+//  APIEndpoint.swift
+//  VaccineCenter
+//
+//  Created by Doyoung on 2022/10/09.
+//
+
+import Foundation
+
+struct APIEndpoint {
+    static func getVaccineCenter(page: String, perPage: String) -> Endpoint<VaccineCenterModel.Response> {
+        return Endpoint(path: "", method: .get, queryParameters: ["page": page, "perPage": perPage])
+    }
+}
