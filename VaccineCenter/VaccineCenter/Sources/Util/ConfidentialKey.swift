5c7447829746ce13abc6264bfef68d22cc83ebc2
iDoyoung <ido.young@icloud.com>
2022-10-10 17:50:44 +0900

    Add comments

--- VaccineCenter/VaccineCenter/Sources/Util/ConfidentialKey.swift
+++ VaccineCenter/VaccineCenter/Sources/Util/ConfidentialKey.swift
@@ -13,9 +13,10 @@ enum ConfidentialKey {
         static let value = getServiceKey()
     }
     
+    ///서비스 키는 Config 파일을 생성해서 Git Ignore 시켰습니다. Clone할 경우, Config file 생성 혹은 인증키를 return 해야합니다.
     private static func getServiceKey() -> String {
         guard let key = Bundle.main.object(forInfoDictionaryKey: "HEADER_AUTH_KEY") as? String else {
-            return  ""
+            return  ""//TODO: 인증키 등록
         }
         return key
     }
